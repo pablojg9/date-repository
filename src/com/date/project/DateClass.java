a8ed67825d10367a7d197997ee521bb1217b2cb3
pablojg9 <pablojuniorgn2@gmail.com>
2021-07-05 12:32:05 -0300

    Date - 05/07/2021

--- src/com/date/project/DateClass.java
+++ src/com/date/project/DateClass.java
@@ -6,8 +6,13 @@ public class DateClass {
     public static void main(String[] args) {
         Date date = new Date();
 
-        System.out.println("Dia da mês: " + date.getMonth());
+        System.out.println("Data em milisegundos: " + date.getTime());
+        System.out.println("Dia da mês: " + date.getDate());
+        System.out.println("Dia da semana: " + date.getDay());
+        System.out.println("Horas do dia: " + date.getHours());
 
+        System.out.println("Minutos em hora: " + date.getMinutes());
+        System.out.println("segundos: " + date.getSeconds());
 
     }
 }
