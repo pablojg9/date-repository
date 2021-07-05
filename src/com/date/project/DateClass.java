34b1f199c46c433a8143ed21eed407e03fc616bc
pablojg9 <pablojuniorgn2@gmail.com>
2021-07-05 12:27:35 -0300

    Date - 05/07/2021

---
+++ src/com/date/project/DateClass.java
@@ -0,0 +1,13 @@
+package com.date.project;
+
+import java.util.Date;
+
+public class DateClass {
+    public static void main(String[] args) {
+        Date date = new Date();
+
+        System.out.println("Dia da mês: " + date.getMonth());
+
+
+    }
+}
