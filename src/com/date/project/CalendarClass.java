3094210e259468c862e2b26e59de36badc692eb6
pablojg9 <pablojuniorgn2@gmail.com>
2021-07-08 12:26:57 -0300

    Date - 08/07/2021

--- src/com/date/project/CalendarClass.java
+++ src/com/date/project/CalendarClass.java
@@ -1,17 +1,27 @@
 package com.date.project;
 
+import javax.swing.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarClass {
-    public static void main(String[] args) {
-        Calendar calendar = Calendar.getInstance();
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
-        System.out.println("Milisegudos: " + calendar.getTimeInMillis());
+        Date dateVencimento = simpleDateFormat.parse("15/07/2021");
+
+        Date dateAtual = simpleDateFormat.parse("20/07/2021");
 
 
-        System.out.println("Dia do mês: " + Calendar.DAY_OF_MONTH);
+        if (dateVencimento.after(dateAtual)){
+            JOptionPane.showMessageDialog(null, "Boleto gerado!");
+        } else {
+            JOptionPane.showMessageDialog(null, "A data do boleto expirou!");
+        }
 
-        System.out.println("Dia da semana: " + Calendar.DAY_OF_WEEK);
+
 
 
     }
