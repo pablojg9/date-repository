1a249ba783fcb400375281a9128810c7ad92aeb0
pablojg9 <pablojuniorgn2@gmail.com>
2021-07-08 15:10:46 -0300

    Date - 08/07/2021

---
+++ src/com/date/project/DateChronoUnit.java
@@ -0,0 +1,22 @@
+package com.date.project;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class DateChronoUnit {
+    public static void main(String[] args) throws ParseException {
+       // Date date = new SimpleDateFormat("dd/MM/yyyy").parse("01/07/2021");
+
+        long dias = ChronoUnit.DAYS.between(LocalDate.parse("2021-07-01"), LocalDate.now());
+
+        System.out.println("Possui " + dias + " dias durante o dia " + LocalDate.now());
+
+
+    }
+
+}
